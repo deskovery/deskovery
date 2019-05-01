975e26d8a208f610299ad173e601959116510bbe
Jess Wallin <JessWallin@gmail.com>
2019-05-01 15:48:44 -0400

    video player render test.

--- client/components/user-home.js
+++ client/components/user-home.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
+import VideoSnapshot from 'video-snapshot'
 
 /**
  * COMPONENT
@@ -8,16 +9,47 @@ import {connect} from 'react-redux'
 export const UserHome = props => {
   const {email} = props
 
+  //   return (
+  //     <div>
+  //       <h3>Welcome, {email}</h3>
+
+  //     </div>
+  //   )
+  // }
+
+  /**
+   * CONTAINER
+   */
+
+  const videoSrc = `https://www.youtube.com/embed/z5F1a7_dsrs`
+
+  const onChange = async e => {
+    const snapshoter = new VideoSnapshot(e.target.files[0])
+    const previewSrc = await snapshoter.takeSnapshot()
+    const img = document.createElement('img')
+
+    img.src = previewSrc
+
+    document.body.appendChild(img)
+  }
+
+  document.getElementById('video').addEventListener('click', onChange)
+
+  // document.querySelector('iframe').addEventListener('change', onChange)
+
   return (
     <div>
-      <h3>Welcome, {email}</h3>
+      <div className="ui embed">
+        <iframe title="video player" id="video" src={videoSrc} />
+      </div>
+      {/* <div className="ui segment">
+          <h4 className="ui header">{video.snippet.title}</h4>
+          <p>{video.snippet.description}</p> */}
     </div>
+    // </div>
   )
 }
 
-/**
- * CONTAINER
- */
 const mapState = state => {
   return {
     email: state.user.email
