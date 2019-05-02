2c7b63c25c07d55baa350e65469cab2b1ca60011
Jess Wallin <JessWallin@gmail.com>
2019-05-02 11:25:15 -0400

    Capture not working, but clean version for error troubleshooting.

--- client/components/user-home.js
+++ client/components/user-home.js
@@ -20,14 +20,57 @@ class UserHome extends Component {
     this.onCapture = this.onCapture.bind(this)
   }
 
-  onCapture() {
-    console.log(captureVideoFrame)
-    const video = this.player.getInternalPlayer()
-    console.log('video', video)
-    const frame = captureVideoFrame('video', 'png')
-    console.log('frame', frame)
-    this.setState({image: frame.dataUri})
+  // onCapture() {
+  //   console.log(captureVideoFrame)
+  //   const video = this.player.getInternalPlayer()
+  //   console.log('video', video)
+  //   const frame = captureVideoFrame('video', 'png')
+  //   console.log('frame', frame)
+  //   this.setState({image: frame.dataUri})
+  // }
+
+  onCapture(video, config) {
+    console.log(video)
+    var canvas = document.createElement('canvas')
+    canvas.width = video.clientWidth
+    canvas.height = video.clientHeight
+
+    var ctx = canvas.getContext('2d')
+    ctx.drawImage(video, 0, 0, canvas.width, canvas.height)
+
+    var mime = {
+      png: 'image/png',
+      jpg: 'image/jpeg'
+    }[(config && config.type) || 'png']
+
+    return canvas.toDataURL(mime)
+    // }
+    // var canvas = document.createElement('canvas')
+    // var video = document.querySelector('video')
+    // var ctx = canvas.getContext('2d')
+
+    // // Change the size here
+    // canvas.width = parseInt(video.width)
+    // canvas.height = parseInt(video.height)
+    // ctx.drawImage(video, 0, 0, canvas.width, canvas.height)
+
+    // window.open(canvas.toDataURL('image/jpeg'))
   }
+
+  // var canvas = document.getElementById('canvas')
+  // var video = document.querySelector('video')
+  // console.log(video)
+  // // canvas.width = video.videoWidth
+  // // canvas.height = video.videoHeight
+  // canvas
+  //   .getContext('2d')
+  //   // .drawImage(video, 0, 0, video.videoWidth, video.videoHeight)
+  //   .drawImage(video, 0, 0)
+  // canvas.toBlob(function(blob) {
+  //   const img = new Image()
+  //   img.src = window.URL.createObjectUrl(blob)
+  // })
+  // }
   // const {email} = props
   ref = youtube => {
     this.player = youtube
@@ -37,13 +80,30 @@ class UserHome extends Component {
     let player = null
     return (
       <div>
+        <canvas id="canvas" />
         <YouTubePlayer
-          url="https://www.youtube.com/watch?v=z5F1a7_dsrs"
+          url="https://www.youtube.com/watch?v=z5F1a7_dsrs&&origin=localhost8080"
           playing={this.state.playing}
           ref={this.ref}
+          config={{
+            file: {
+              attributes: {
+                crossorigin: 'anonymous'
+              }
+            }
+          }}
           controls
         />
-        <button type="button" onClick={this.onCapture}>
+        <button
+          type="button"
+          onClick={() => {
+            const frame = captureVideoFrame(
+              this.player.player.getInternalPlayer()
+            )
+            console.log('captured frame', frame)
+            this.setState({image: frame.dataUri})
+          }}
+        >
           {' '}
           Capture{' '}
         </button>
